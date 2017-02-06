242372df06174ae1b4b8995d5847264a10ae866c
Filip Filipovic <filip@webbite.de>
2017-02-06 14:52:18 +0000

    Fixed formulare publishing command

--- app/Console/Commands/DocumentsPublishForms.php
+++ app/Console/Commands/DocumentsPublishForms.php
@@ -173,4 +173,19 @@ class DocumentsPublishForms extends Command
         return $document;
     }
     
+    /**
+     * Generate Unique URL String
+     * @return bool 
+     */
+    private function generateUniqeLink($length=6){
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < $length; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); //turn the array into a string
+    }
+    
 }
